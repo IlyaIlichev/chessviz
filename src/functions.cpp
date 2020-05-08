762b0398766335e0a6a9a94f57d1dd87a56d7237
IlyaIlichev <ilia.ilichev@mail.ru>
2020-05-08 13:07:58 +0700

    Move table draw to separate function.

---
+++ src/functions.cpp
@@ -0,0 +1,11 @@
+#include "functions.h"
+
+
+void drawtable(){
+  for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+          cout << "  " << table[i][j];
+      }
+      cout << endl;
+  };
+}
